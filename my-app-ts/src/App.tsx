95e74cdc70ba44338e9b08751dc2c84290d546bf
ImNotaStrawberry <samucasantana2003@gmail.com>
2023-07-28 15:39:08 -0300

    Styles

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,28 +1,32 @@
 import {Layout} from './components/Layout'
-import {Card} from './components/Card'
+import styled from 'styled-components'
+import { 
+  ChakraProvider,
+  Input,
+  Box,
+  Center,
+  Button
+} from '@chakra-ui/react'
+
 
 function App() {
   return (
-    <Layout>
-      Hello World
-      <Card 
-      id={1}
-      paragraph='TypeScript' 
-      details='TS para front-end e back-end'
-      />
-
-      <Card 
-      id={2} 
-      paragraph='HTML'
-      details='HTML para front-end'
-      />
-
-      <Card 
-      id={3} 
-      paragraph='SQL'
-      details='SQL para banco de dados'
-      />
-    </Layout>
+    <ChakraProvider>
+      <Box minHeight='100vh' backgroundColor='black' padding='25px'>
+        <Box height={250} backgroundColor='white' borderRadius='25px' padding='15px' display='flex' flexDirection='column' justifyContent='space-around'>
+          <Center>
+            <h1>Faça o login</h1>
+          </Center>
+          <Input placeholder='email'/>
+          <Input placeholder='password'/>
+          <Center>
+            <Button colorScheme='teal' size='md'>
+              Button
+            </Button>
+          </Center>
+        </Box>
+      </Box>
+    </ChakraProvider>
   );
 }
 
